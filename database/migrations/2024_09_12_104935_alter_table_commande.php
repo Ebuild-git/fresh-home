c6aaed47552630a0727e0f82241e5656cdefe8f4
kg <48618359+kevingassam@users.noreply.github.com>
2024-09-12 06:50:20 -0400

    ajouter les frais

---
+++ database/migrations/2024_09_12_104935_alter_table_commande.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('commandes', function (Blueprint $table) {
+            $table->decimal("frais", 10,3)->default(0);
+            $table->decimal("tva", 13, 3)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('commandes', function (Blueprint $table) {
+            $table->dropColumn('frais');
+            $table->dropColumn('tva');  
+        });
+    }
+};

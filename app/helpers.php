f5bf845ee547d43349e3652137053875b255f002
kg <48618359+kevingassam@users.noreply.github.com>
2024-09-22 21:05:49 -0400

    traduction

--- app/helpers.php
+++ app/helpers.php
@@ -8,15 +8,20 @@ class TranslationHelper
 {
     public static function TranslateText($text)
     {
-        $locale = app()->getLocale(); 
-        
-        if($locale == "en"){
-            $tr = new GoogleTranslate($locale);
-            return $tr->translate($text);
-        }else{
+        $locale = app()->getLocale();
+
+        // Si la langue est déjà "fr", pas besoin de traduire
+        if ($locale == "fr") {
             return $text;
         }
-        
-       
+
+        // Créer une clé unique pour chaque texte et langue
+        $cacheKey = 'translation_' . md5($text . '_' . $locale);
+
+        // Vérifier si la traduction est déjà en cache
+        return cache()->remember($cacheKey, now()->addDays(7), function () use ($text, $locale) {
+            $tr = new GoogleTranslate($locale);
+            return $tr->translate($text);
+        });
     }
 }
